78321ffdbcc730d96dc461c3cbb4246522ead3b9
cauetrainotti <trainotti.caue@gmail.com>
2025-12-24 17:07:25 -0300

    feat: implementar factory para seleção de leiaute

---
+++ ConsoleApp1/GeradorTxt/GeradorArquivoFactory.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleApp1.GeradorTxt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorTxt
+{
+    public class GeradorArquivoFactory
+    {
+        public static GeradorArquivoBase Criar(LeiauteTipo tipo)
+        {
+            switch (tipo)
+            {
+                case LeiauteTipo.Leiaute1:
+                    return new GeradorArquivoLeiaute01();
+
+                case LeiauteTipo.Leiaute2:
+                    return new GeradorArquivoLeiaute02();
+
+                default:
+                    throw new ArgumentOutOfRangeException(
+                        nameof(tipo),
+                        tipo,
+                        "Leiaute desconhecido"
+                    );
+            }
+        }
+    }
+}
