a9547e093bbf22e76d2090de454ee90aaca30068
cauetrainotti <trainotti.caue@gmail.com>
2025-12-24 16:54:33 -0300

    feat: implementar leiaute 1 como implementação padrão

---
+++ ConsoleApp1/GeradorTxt/GeradorArquivoLeiaute01.cs
@@ -0,0 +1,13 @@
+﻿using GeradorTxt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.GeradorTxt
+{
+    public class GeradorArquivoLeiaute01 : GeradorArquivoBase
+    {
+    }
+}
